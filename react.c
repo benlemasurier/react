9785c6af720200a1d8129d2b0faedf7593019cbe
Ben LeMasurier <ben.lemasurier@gmail.com>
2012-09-26 11:48:14 -0600

    syslogging, daemonize

--- react.c
+++ react.c
@@ -11,11 +11,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 #include <errno.h>
 #include <unistd.h>
+#include <syslog.h>
 #include <sys/inotify.h>
 #include <sys/select.h>
 #include <sys/signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "config.h"
 
 #define EVENT_SIZE    (sizeof(struct inotify_event))
@@ -24,8 +29,11 @@
 struct react {
   int fd;
   int wd;
+  int logging;
   char *file;
   char *command;
+  char *progname;
+  bool foreground;
 } react;
 
 void
@@ -43,6 +51,9 @@ quit(void)
 
   if(react.fd)
     close(react.fd);
+
+  syslog(LOG_NOTICE, "shutting down");
+  closelog();
 }
 
 void
@@ -88,23 +99,65 @@ watch(void)
 static void
 usage(void)
 {
-  printf("react <file to watch> <command to run>");
+  printf("%s [-fv] <file to watch> <command>", react.progname);
   exit(EXIT_SUCCESS);
 }
 
 int
 main(int argc, char **argv)
 {
+  int arg;
+  pid_t pid, sid;
+
+  /* defaults */
   memset(&react, 0, sizeof(struct react));
+  react.progname = argv[0];
+  react.logging = LOG_CONS | LOG_PID | LOG_NDELAY;
+  react.foreground = false;
 
   /* quit cleanly */
   atexit(quit);
   signal(SIGINT, sigquit);
 
+  while((arg = getopt(argc, argv, "fv")) != -1) {
+    switch(arg) {
+      case 'v':
+        react.logging |= LOG_PERROR;
+        break;
+      case 'f':
+        react.foreground = true;
+        break;
+      case '?':
+        if(optopt == 'h')
+          fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+        else if(isprint(optopt))
+          fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+        else
+          fprintf(stderr, "Unknown option '\\x%x'\n", optopt);
+    }
+  }
+
   /* command-line options */
   if(argc < 3) usage();
-  react.file    = argv[1];
-  react.command = argv[2];
+  react.file    = argv[argc - 2];
+  react.command = argv[argc - 1];
+
+  /* logging */
+  openlog(react.progname, react.logging, LOG_LOCAL3);
+
+  /* daemonize? */
+  if(!react.foreground) {
+    pid = fork();
+    if(pid < 0)
+      exit(EXIT_FAILURE);
+    else if(pid > 0)
+      exit(EXIT_SUCCESS);
+
+    umask(0);
+    sid = setsid();
+    if(sid < 0)
+      exit(EXIT_FAILURE);
+  }
 
   /* initialize inotify */
   if((react.fd = inotify_init()) < 0)
